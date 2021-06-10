61fa09851b9a6ac8781b1fda1f373b1f4e227936
fm247 <hiyoo95@gmail.com>
2021-06-10 09:56:40 +0900

    practice on str slicing

---
+++ str_ex/str_slicing.py
@@ -0,0 +1,12 @@
+a = '20210610Sunny'
+year = a[:4]
+print(year)
+month = a[4:6]
+print(month)
+day = a[6:8]
+print(day)
+weather = a[8:]
+print(weather)
+
+b = 'Pithon' # => Python
+print(b[:1] + 'y' + b[2:])

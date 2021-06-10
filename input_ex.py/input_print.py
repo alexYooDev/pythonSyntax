a1820fdd32ca6a241031179da65cb3276b93acd8
fm247 <hiyoo95@gmail.com>
2021-06-10 21:52:28 +0900

    added input_print examples 2021-0610-2152

---
+++ input_ex.py/input_print.py
@@ -0,0 +1,13 @@
+#print() changes line after output
+
+a = 1
+b = 2
+
+print(a,b)
+print(7, end=" ")
+print(8, end=" ")
+
+answer = 7
+print("The answer is "+str(answer))
+
+print(f'The answer is {answer}')
